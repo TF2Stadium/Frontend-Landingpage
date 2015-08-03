8099f2c58f64eca2e5b6ff9765d4ea1f63618016
Capu <mariokapu@gmail.com>
2015-08-03 20:31:35 +0200

    First commit

---
+++ js/scroll.js
@@ -0,0 +1,151 @@
+var options = [
+  {selector: '#contact-list', offset: 100, callback: "showStaggeredChildren('#contact-list', 'li', 'bottom')" },
+  {selector: '#statement-text', offset: 100, callback: "showStaggeredChildren('#statement', 'p, h1, h2', 'top')" },
+  {selector: '.screenshots', offset: 100, callback: "showStaggeredChildren('.screenshots', 'img', '')" },
+  {selector: '#video', offset: 100, callback: "showStaggeredChildren('#video', 'video', 'top')" }
+];
+
+$( document ).ready(function() {
+    var toAnimate = ['#statement-text *', '#statement h2', 'video', 'li', '.screenshots img'];
+    for (var i=0; i < toAnimate.length; i++) {
+      selector = toAnimate [i];
+      $(selector).addClass('hidden');
+    }
+});
+
+Materialize.scrollFire(options);
+
+showStaggeredChildren = function(selector, children, direction, onEnd) {
+  var time = 0;
+  switch (direction) {
+    case 'bottom':
+    translateY = "100px"; translateX = "0";
+    break;
+    case 'top':
+    translateY = "-100px"; translateX = "0";
+    break;
+    case 'right':
+    translateY = "0"; translateX = "100px";
+    break;
+    case 'left':
+    translateY = "0"; translateX = "-100px";
+    break;
+    default: //left
+    translateY = "0"; translateX = "0px";
+  }
+
+  $(selector).find(children).velocity(
+      { translateY: translateY, translateX: translateX, opacity: 0},
+      { duration: 0 });
+
+  $(selector).find(children).each(function() {
+    $(this).velocity(
+      { opacity: "1", translateY: "0", translateX: "0"},
+      { duration: 400, delay: time, easing: 'easeOutCubic',
+        complete: function() {
+          if (children != 'img') return;
+          $(this).addClass('materialboxed');
+          $('.materialboxed').materialbox();
+          }
+        });
+    time += 120;
+  });
+};
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate(
+          {scrollTop: target.offset().top},
+          {
+            duration: 400,
+            easing: 'easeOutCubic'
+          });
+        return false;
+      }
+    }
+  });
+});
+
+/*
+ * jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
+ *
+ * Uses the built in easing capabilities added In jQuery 1.1
+ * to offer multiple easing options
+ *
+ * TERMS OF USE - jQuery Easing
+ *
+ * Open source under the BSD License.
+ *
+ * Copyright Â© 2008 George McGinley Smith
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list
+ * of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ *
+ * Neither the name of the author nor the names of contributors may be used to endorse
+ * or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+*/
+
+// t: current time, b: begInnIng value, c: change In value, d: duration
+jQuery.easing['jswing'] = jQuery.easing['swing'];
+
+jQuery.extend( jQuery.easing,
+{
+	easeOutCubic: function (x, t, b, c, d) {
+		return c*((t=t/d-1)*t*t + 1) + b;
+	}
+});
+
+/*
+ *
+ * TERMS OF USE - EASING EQUATIONS
+ *
+ * Open source under the BSD License.
+ *
+ * Copyright Â© 2001 Robert Penner
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list
+ * of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ *
+ * Neither the name of the author nor the names of contributors may be used to endorse
+ * or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
